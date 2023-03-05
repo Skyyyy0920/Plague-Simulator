19a55e4688eab9a014e4131d9df13a8d1916e448
Tianhao Alexander Huang <huangtianhao0920@126.com>
2023-03-05 13:21:27 +0800

    commit
    Signed-off-by: Tianhao Alexander Huang <huangtianhao0920@126.com>

---
+++ run.py
@@ -0,0 +1,54 @@
+import simulation
+from PyQt5.QtWidgets import *
+import sys
+from params import *
+from refresh import *
+import settings
+from drawing import *
+from personpool import *
+from receiver import *
+from transmission import *
+import pygame
+
+
+def init_hospital():
+    Hospital()
+
+
+def init_refected():
+    persons = Persons().persons  # 获取所有的市民
+    for i in range(0, Params.original_infected_count):
+        person = None
+        while True:
+            person = persons[random.randrange(0,len(persons))] # 随机选择一个市民
+            # 如果该市民没有被感染，则选中该市民
+            if not person.is_infected():
+                break
+        person.be_infected()   # 让该市民被感染
+
+
+if __name__ == '__main__':
+
+    # pygame.init()
+    pygame.mixer.init()
+    music = pygame.mixer.Sound(file="环太平洋.mp3")
+    music.play(loops=-1)
+    music.set_volume(0.4)
+
+    app = QApplication(sys.argv)
+    Params.app = app
+    mainWindow = QMainWindow()
+    ui = simulation.Ui_mainWindow()
+    ui.setupUi(mainWindow)
+    init_hospital()
+    init_refected()
+    drawing = Drawing(ui)
+
+    refresh = Refresh(drawing)
+    refresh.start()
+
+    receiver = Receiver()
+    receiver.start()
+    mainWindow.show()
+
+    sys.exit(app.exec_())
